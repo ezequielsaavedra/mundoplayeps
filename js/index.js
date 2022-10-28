8bc05df91cd667c28f78dcce2106a3c94f1dc559
ezequiel <saavedraeze66@gmail.com>
2022-10-28 17:12:07 -0300

    arreglos varios

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 alert("Bienvenido a Mundo Play");
 
-let compra = parseInt(prompt("Que item desea comprar? 1:Bloodborne 2:God of War 3:Horizon 4:Days Gone 5:Call of Duty"));
+let compra = parseInt(prompt("Que item desea comprar? \n1:Bloodborne 2:God of War 3:Horizon 4:Days Gone 5:Call of Duty"));
 
-let carrito = 0 ,seguirComprando = true ,continuarCompra, cuotas; 
-
-console.log (typeof carritoDividido)
+let carrito = 0 ,seguirComprando = true ,continuarCompra;
 
 agregarCarrito = (total, producto) => {
     resultado = total + producto
@@ -23,30 +21,16 @@ while (seguirComprando === true) {
     } else if (compra === 5) {
         carrito = agregarCarrito (carrito, 30)
     } else {
-        compra = parseInt(prompt("Por favor, elija un producto de nuestro catalogo: 1:Bloodborne 2:God of War 3:Horizon 4:Days Gone 5:Call of Duty"))
+        compra = parseInt(prompt("Por favor, elija un producto de nuestro catalogo: \n1:Bloodborne 2:God of War 3:Horizon 4:Days Gone 5:Call of Duty"))
         continue
-    }
-    continuarCompra = prompt("Quiere seguir comprando? Si o No?").toLowerCase()
-    if (continuarCompra === "si") {
-        compra = parseInt(prompt("Que item desea comprar? 1:Bloodborne 2:God of War 3:Horizon 4:Days Gone 5:Call of Duty")) 
+    };
+    continuarCompra = parseInt(prompt("Quiere seguir comprando? \n1:Si o 2:No?"))
+    if (continuarCompra === 1) {
+        compra = parseInt(prompt("Que item desea comprar? \n1:Bloodborne 2:God of War 3:Horizon 4:Days Gone 5:Call of Duty")) 
     } else { 
-        alert ("El total de su compra es " + carrito + " U$D. Gracias por comprar con nostros!!")
+        alert ("El total de su compra es " + carrito + " U$D. \nGracias por comprar con nostros!!")
         seguirComprando = false
-        // while (seguirComprando === true) {
-        // cuotas = parseInt(prompt("El total de su compra es " + carrito + " U$D. En cuantas cuotas desea abonar? 1 - 3 - 6 -12"))
-        //     if ((cuotas === 1) || (cuotas === 3) || (cuotas === 6) || (cuotas === 12)) {
-        //         alert ("Su total es " + carrito / cuotas + " U$D a abonar en "+ cuotas + " cuota/s. Gracias por elegirnos!!")
-        //         seguirComprando = false
-        //     } 
-        //     else {
-        // cuotas = parseInt(prompt("Por favor, seleccione una cantidad permitida: 1 - 3 - 6 -12"))
-        //     continue
-        //     }
-        // }
-    // } else {
-    //     prompt("Quiere seguir comprando? Si o No?").toLowerCase()
-    //     continue
-    }
+    };
 };
 
 
