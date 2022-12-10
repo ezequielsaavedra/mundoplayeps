e86dfe27386f5308f5c80ed5399465c5173214c0
ezequiel <saavedraeze66@gmail.com>
2022-12-10 09:23:32 -0300

    buscador y filtro compuesto

--- js/index.js
+++ js/index.js
@@ -4,26 +4,49 @@ const userNombre = document.getElementById("inputNombre");
 const userApellido = document.getElementById("inputApellido");
 const userEmail = document.getElementById("inputEmail");
 
-$("#btnIngresar").click(function(){
-    const user = {
-        nombre: userNombre.value,
-        apellido: userApellido.value,
-        email: userEmail.value,
-    }
+const iniciarSesion = () => {
+    if ((localStorage.getItem("user"))){
+        const user = JSON.parse(localStorage.getItem("user"));
+        userNombre.value = user.nombre
+        userApellido.value = user.apellido
+        userEmail.value = user.email
+        $("#btnIngresar").click(function(){
+        const user = {
+            nombre: userNombre.value,
+            apellido: userApellido.value,
+            email: userEmail.value,
+        }
     const userJSON = JSON.stringify(user)
     localStorage.setItem("user",userJSON)
-    if (localStorage.getItem("user")) {
         $(".login").hide();
         $(".hidden").show();
+        $("#filtroConsola").hide();
         $(".Producto-estandar").hide();
-    }
-})
+    })
+    } else {$("#btnIngresar").click(function(){
+            const user = {
+                nombre: userNombre.value,
+                apellido: userApellido.value,
+                email: userEmail.value,
+            }
+        const userJSON = JSON.stringify(user)
+        localStorage.setItem("user",userJSON)   
+            $(".login").hide();
+            $(".hidden").show();
+            $("#filtroConsola").hide();
+            $(".Producto-estandar").hide();
+    })}
+}
+
+iniciarSesion ();
 
 // body
 
 const juegos = document.getElementById("juegos");
 const carritoOffcanvas = document.getElementById("carrito");
 const totalCompra = document.getElementById("totalCompra");
+const iva = document.getElementById("iva");
+const precioFinal = document.getElementById("precioFinal");
 const contador = document.getElementById("contador");
 const formComprar = document.getElementById("formComprar");
 const arrayProductos = [];
@@ -37,40 +60,51 @@ const obtenerProductos = async () => {
     return arrayProductos
 }
 
-obtenerProductos().then(arrayProductos => {
-    arrayProductos.forEach ((p) => {
-        juegos.innerHTML +=
-        `
-        <div class="card border-light mb-3 col-4 producto ${p.categoria} ${p.categoriaDos} ${p.consola}" id="producto${p.id}">
-        <img src= ${p.imagen} class="card-img-top" alt= ${p.nombre}, ${p.consola}> 
-        <div class="card-body d-flex justify-content-around">
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#agregarCarritoModal${p.id}">Comprar</button>
-            <p class="card-text align-self-center">$${p.precio}</p>
-        </div>
-        </div>
-        <div class="modal fade" id="agregarCarritoModal${p.id}" tabindex="-1" aria-labelledby="agregarCarritoModalLabel${p.id}" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-            <div class="modal-body">
-                <p> Desea agregar ${p.nombre} al carrito?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick= "selecProd(arrayProductos[${p.id}],${p.id})" id="agregarCarritoProducto${p.id}">Si</button>
-                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">No</button>
+const crearCards = () => {
+    obtenerProductos().then(arrayProductos => {
+        arrayProductos.forEach (p => {
+            juegos.innerHTML +=
+            `
+            <div class="card border-light mb-3 col-4 producto ${p.categoria} ${p.categoriaDos} ${p.consola} ${p.className}" id="producto${p.id}">
+            <img src= ${p.imagen} class="card-img-top" alt= ${p.nombre}, ${p.consola}> 
+            <div class="card-body">
+            <h5  class="card-title">${p.nombre}</h5>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#agregarCarritoModal${p.id}">Comprar</button>
+                <p class="card-text align-self-center pt-3">Precio: $${p.precio}</p>
+                <p class="card-text align-self-center" id="cantidad${p.id}">Cantidad disponible: x${p.stock}</p>
             </div>
             </div>
-        </div>
-        </div>
-        `
-})
+            <div class="modal fade" id="agregarCarritoModal${p.id}" tabindex="-1" aria-labelledby="agregarCarritoModalLabel${p.id}" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                <div class="modal-body">
+                    <p> Desea agregar ${p.nombre} al carrito?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick= "selecProd(arrayProductos[${p.id}],${p.id})">Si</button>
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">No</button>
+                </div>
+                </div>
+            </div>
+            </div>
+            `
+    })
+    }
+    );
 }
-);
+
+crearCards();
 
 const carrito = [];
+
+const cambiarCantidad = (prod) => {
+        document.getElementById(`cantidad${prod.id}`).innerText = `Cantidad disponible: x${prod.stock}`    
+}
 
 const selecProd = (prod, id) => {
     if (prod.stock >= 1) {
         prod.stock--
+        cambiarCantidad (prod);
         if (carrito.find((prod) => prod.id === id)) {
             prod.cantidad++
         } else {
@@ -78,7 +112,6 @@ const selecProd = (prod, id) => {
         }
         calcularPrecio ();
         contadorCarrito ();
-        console.log(carrito)
     } else {
         Swal.fire({
             icon: 'error',
@@ -94,12 +127,15 @@ const agregarCarrito = () => {
         carritoOffcanvas.innerHTML = ""
         formComprar.innerHTML = ""
         actualizarCarrito()
+        calcularPrecio()
     }
     )
 }
 
 const eliminarCarrito = (id) => {
         const productoEliminar = carrito.find((p) => p.id === id);
+        productoEliminar.stock++
+        cambiarCantidad (productoEliminar);
         if (productoEliminar.cantidad > 1){
             productoEliminar.cantidad--
         } else {
@@ -134,8 +170,12 @@ const actualizarCarrito = () =>{
 }
 
 const calcularPrecio = () => {
-    const precioTotal = carrito.reduce((total, e) => total + e.precio*e.cantidad,0)
-    totalCompra.innerText = `Su total es de: $${precioTotal}`  
+    const precioSinIva = carrito.reduce((total, e) => total + e.precio*e.cantidad,0)
+    const precioIva = precioSinIva*.21
+    const precioMasIva = precioSinIva + precioIva
+    totalCompra.innerText = `El monto es de: $${precioSinIva}`
+    iva.innerText = `+ IVA: $${precioIva}`
+    precioFinal.innerText = `Su total es de: $${precioMasIva}`
 }
 
 const contadorCarrito = () => {
@@ -176,6 +216,22 @@ const finalizarCompra = () => {
     }
 }
 
+const vaciarCarrito = () => {
+    if(carrito.length >= 1) {
+        carrito.forEach (p=> {
+            p.stock = p.stock + p.cantidad
+            cambiarCantidad (p);
+            p.cantidad = 1
+        })
+        carrito.splice (0, carrito.length)
+            carritoOffcanvas.innerHTML = ""
+        formComprar.innerHTML = ""
+        actualizarCarrito ();
+        calcularPrecio ();
+        contadorCarrito ();
+    }
+}
+
 const aceptarCompra = () => {
     const comprador = JSON.parse(localStorage.getItem("user"));
     Swal.fire({
@@ -187,24 +243,36 @@ const aceptarCompra = () => {
     })
     carritoOffcanvas.innerHTML = ""
         formComprar.innerHTML = ""
+        carrito.forEach (p => {
+            p.cantidad = 1
+        })
         carrito.splice(0,carrito.length)
         calcularPrecio ();
         contadorCarrito ();
-}
+}   
 
 agregarCarrito ();
 
 finalizarCompra ();
 
-//filtros productos
+//filtros
 
-const agregarFiltros = (btn, prod) => {
+const agregarFiltrosCategorias = (btn, prod) => {
     $(btn).change(function(){
         $(".producto").hide();
         $(prod).toggle();
         $("#eliminarFiltros").show();
+        $("#filtroConsola").show();
+        $(".consolaButtons").prop("checked", false);
     }
     )
+}
+
+const agregarFiltrosConsolas = (btn, prod) => {
+    $(btn).change(function(){
+        $(".producto").hide();
+        $(prod).toggle();
+})
 }
 
 const eliminarFiltros = () => {
@@ -213,15 +281,46 @@ const eliminarFiltros = () => {
         $(".filtrosButton").prop("checked", false);
         $(".Producto-destacado").show();
         $("#eliminarFiltros").hide();
+        $("#filtroConsola").hide();
     }
     )
 }
 
 eliminarFiltros ();
 
-agregarFiltros ("#Accion",".Accion");
-agregarFiltros ("#Aventura", ".Aventura");
-agregarFiltros ("#Disparos", ".Disparos");
-agregarFiltros ("#Deportes", ".Deportes");
-agregarFiltros ("#PS4",".PS4");
-agregarFiltros ("#PS5",".PS5");
+agregarFiltrosCategorias ("#Accion",".Accion");
+agregarFiltrosCategorias ("#Aventura", ".Aventura");
+agregarFiltrosCategorias ("#Disparos", ".Disparos");
+agregarFiltrosCategorias ("#Deportes", ".Deportes");
+agregarFiltrosConsolas ("#PS4", ".Accion-PS4");
+agregarFiltrosConsolas ("#PS4", ".Aventura-PS4");
+agregarFiltrosConsolas ("#PS4", ".Disparos-PS4");
+agregarFiltrosConsolas ("#PS4", ".Deportes-PS4");
+agregarFiltrosConsolas ("#PS5", ".Accion-PS5");
+agregarFiltrosConsolas ("#PS5", ".Aventura-PS5");
+agregarFiltrosConsolas ("#PS5", ".Disparos-PS5");
+agregarFiltrosConsolas ("#PS5", ".Deportes-PS5");
+
+//buscador
+
+const inputBuscar = document.getElementById("inputBuscar");
+
+const searchBar = () => {
+    $("#buttonBuscar").click(function(){
+        $(".producto").hide();
+        $("#eliminarFiltros").show();
+        const arrayBuscar = arrayProductos.filter((p)=> p.className.includes(`${inputBuscar.value.toLowerCase()}`))
+        const mostrarBuscador = arrayBuscar.forEach(p=> {
+            $(`.${p.className}`).show();
+        })
+    }
+    )
+}
+
+$(".searchBar").keypress(function(e) {
+    if (e.which == 13) {
+        return false;
+    }
+    });
+
+searchBar();
